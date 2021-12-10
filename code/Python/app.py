be36700d6b781d530cea3c94aa5cec6b3b60136b
Ashley Mutenha <46158817+ashleymutenha@users.noreply.github.com>
2021-12-10 15:22:58 +0200

    intergrated flask with stripe

--- code/Python/app.py
+++ code/Python/app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask, request,jsonify,redirect,render_template
 from flask_cors import CORS
 from flask_classful import FlaskView,route
 from flask_mysqldb import MySQL
 from Details import Products
-import stripe
+import stripe 
+from dotenv import load_dotenv
+import webbrowser
+
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -12,11 +19,11 @@ app.config['MYSQL_USER'] ='root'
 app.config['MYSQL_PASSWORD'] =''
 app.config['MYSQL_DB'] ='flask'
 
-mysql = MySQL(App)
+mysql = MySQL(app)
 
 
 CORS(app)
-
+message ={"Content":""}
 class GateWay(FlaskView):
 
 	def connection(self):
@@ -28,10 +35,48 @@ class GateWay(FlaskView):
 	def db_Connection(self):
 		return Products(self.connection(),self.cursor())
 
-	@route("/",methods =["GET"])
-	def index(self):
-		try:
-			return jsonify(self.db_Connection().)
+	@route("/getProducts",methods =["GET"])#passing products details to angular frontend
+	def getProducts(self):
+		return jsonify(self.db_Connection().getProducts())
+
+	@route("/orderProduct",methods =["GET","POST"])
+	def orderProduct(self): #extracting purchasing request fron angular and making purchase order with stripe
+		if request.method=="POST":
+			data = request.get_json()
+			product = self.db_Connection().extractProduct(data)
+
+			if data!= product["Name"]:
+				abort(400)
+
+			#creating stripe checkout session
+			checkout_session = stripe.checkout.Session.create(
+				line_items=[{
+				'price_data':{
+			    'product_data':{
+			     'name':product["Name"],
+			},
+			'unit_amount':int(product["Price"]),
+			'currency':'usd'
+				},
+				'quantity':1,},],
+			    payment_method_types =["card"],
+			    mode ="payment",
+			    success_url = request.host_url + 'order/success',
+			    cancel_url = request.host_url + 'order/cancel'
+			)
+			return webbrowser.open_new_tab(checkout_session.url)
+	@route('/order/cancel')
+	def cancel(self):#page rendered if payment is not successful
+		return jsonify({"Message":"cancel"})
+
+
+	@route('/order/success')
+	def success(self):#page rendered if payment is successful
+		return jsonify({"Message":"success"})
+
+GateWay.register(app,route_base ="/")
+if __name__=="__main__":
+	app.run(debug=True)
 
 
 	@route("/config")
