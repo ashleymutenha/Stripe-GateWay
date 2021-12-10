db7468543317147c28460a17556a6c3b678e9c1d
Ashley Mutenha <46158817+ashleymutenha@users.noreply.github.com>
2021-12-10 15:36:57 +0200

    Update app.component.ts

--- code/Angular/src/app.component.ts
+++ code/Angular/src/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import {faHome,faBookmark,faToggleOn,faNewspaper} from '@fortawesome/free-solid-svg-icons'
 import {ApiService} from './api.service'
+import {Name} from './interfaces'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,10 +12,11 @@ export class AppComponent {
  faHome =faHome;
  faToggleOn =faToggleOn;
  faNewspaper =faNewspaper
-
+ @Input() data:any;
  public productDetails:any
 
  public currentView ="home"
+ public model = new Name("")
 
 
 constructor(private api:ApiService) { }
@@ -26,5 +28,9 @@ constructor(private api:ApiService) { }
 getProducts(){
   this.api.getProducts().subscribe(data=>this.productDetails=data,
     error=>this.currentView="serverDown")
+}
+
+purchaseProduct(product:any){
+ this.api.postProduct(product.Name).subscribe(error=>this.currentView="serverDown")
 }
 }
